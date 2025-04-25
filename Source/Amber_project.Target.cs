f6e23aac64cead8f62b6938a1a3fcce241a2ccff
Dawn <www.tuio20240190@foxmail.com>
2025-04-25 23:39:38 +0800

    Initial commit: Clean Unreal project with .gitignore

---
+++ Source/Amber_project.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Amber_projectTarget : TargetRules
+{
+	public Amber_projectTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		ExtraModuleNames.Add("Amber_project");
+	}
+}
